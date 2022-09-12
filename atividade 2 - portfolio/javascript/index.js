85406926aec0f5f238fb35829a97d6fe95c93c94
Leonardo Silva <103298909+Leosilvag@users.noreply.github.com>
2022-09-12 14:07:41 -0300

    Delete atividade 2 - portfolio directory

--- atividade 2 - portfolio/javascript/index.js
+++
@@ -1,7 +0,0 @@
-const chk=document.querySelector('.checkbox');
-const body=document.querySelector('body');
-
-chk.onclick=function(){
-    this.classList.toggle('active');
-    body.classList.toggle('active');
-}
